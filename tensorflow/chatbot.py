b9d59e9180359fcffa35b2842e16e89176f8a6b9
Daniel Watson <watsondaniel6@gmail.com>
2017-03-03 19:48:52 +0400

    Added dialogue data parser

--- tensorflow/chatbot.py
+++ tensorflow/chatbot.py
@@ -1,60 +1,63 @@
+import random
+
 import numpy as np
 import tensorflow as tf
 
 
-class DataParser(object):
-  """ADD DOCUMENTATION.
+def parseDialogueData(shuffle_pairs=True):
+  """Returns dialogue pairs from the Cornell Movie dialogue dataset. Produces
+     about 2.2 million of such pairs.
   
-  In all files the field separator is " +++$+++ "
-
-  - movie_titles_metadata.txt
-    - contains information about each movie title
-    - fields: 
-      - movieID, 
-      - movie title,
-      - movie year, 
-        - IMDB rating,
-      - no. IMDB votes,
-      - genres in the format ['genre1','genre2',...,'genreN']
-
-  - movie_characters_metadata.txt
-    - contains information about each movie character
-    - fields:
-      - characterID
-      - character name
-      - movieID
-      - movie title
-      - gender ("?" for unlabeled cases)
-      - position in credits ("?" for unlabeled cases) 
-
-  - movie_lines.txt
-    - contains the actual text of each utterance
-    - fields:
-      - lineID
-      - characterID (who uttered this phrase)
-      - movieID
-      - character name
-      - text of the utterance
-
-  - movie_conversations.txt
-    - the structure of the conversations
-    - fields
-      - characterID of the first character involved in the conversation
-      - characterID of the second character involved in the conversation
-      - movieID of the movie in which the conversation occurred
-      - list of the utterances that make the conversation, in chronological 
-        order: ['lineID1','lineID2',...,'lineIDN']; has to be matched with
-        movie_lines.txt to reconstruct the actual content"""
+     In both files the field separator is " +++$+++ "
+     
+     - movie_lines.txt
+       - contains the actual text of each utterance
+       - fields:
+         - lineID
+         - characterID (who uttered this phrase)
+         - movieID
+         - character name
+         - text of the utterance
+ 
+     - movie_conversations.txt
+       - the structure of the conversations
+       - fields
+         - characterID of the first character involved in the conversation
+         - characterID of the second character involved in the conversation
+         - movieID of the movie in which the conversation occurred
+         - list of the utterances that make the conversation, in chronological 
+           order: ['lineID1','lineID2',...,'lineIDN']; has to be matched with
+           movie_lines.txt to reconstruct the actual content"""
   
-  def __init__(self):
-    pass
+  with open('data/movie_lines.txt') as f:
+    raw_movie_lines = f.readlines()
   
+  movie_lines = {}
+  for line in raw_movie_lines:
+    line_items = line.split(' +++$+++ ')
+    # Remove extra whitespaces, newlines and tabs, and replace right-sided
+    # apostrophes with the regular single quotation mark
+    txt = ' '.join(line_items[-1].split()).replace('\x92', "'")
+    movie_lines[line_items[0]] = txt
+  del raw_movie_lines  # just for memory usage improvement
+  
+  with open('data/movie_conversations.txt') as f:
+    raw_movie_conversations = f.readlines()
+  
+  pairs = []
+  for line in raw_movie_conversations:
+    line_ids = eval(line.split(' +++$+++ ')[-1])
+    for i in range(len(line_ids) - 1):
+      self.pairs.append(
+        (movie_lines[line_ids[i]], movie_lines[line_ids[i+1]]) )
+  
+  if shuffle_pairs:
+    random.shuffle(pairs)
+  return pairs
+
 
 class Seq2SeqModel(object):
   """ADD DOCUMENTATION."""
   
   def __init(self):
     pass
-
-
-
