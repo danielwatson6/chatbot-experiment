60a18191fb02e5c89f49ce74794c8669d8c40f30
Daniel Watson <watsondaniel6@gmail.com>
2017-03-04 13:40:08 +0400

    Using Meteor fs for thread bug fix

--- webapp/server/main.js
+++ webapp/server/main.js
@@ -7,6 +7,8 @@ import '/imports/api/router'
 
 Meteor.startup(() => {
   if (Movies.find().count() === 0) {
+    // Use `fs` instead of importing the JSON itself - reading the data files
+    // is only necessary if there is nothing in the Mongo database
     dataDir = process.cwd() + '/../web.browser/app/movie_metadata.json'
     const data = JSON.parse(fs.readFileSync(dataDir, 'utf8'))
     // Parsing results in stringified indices
